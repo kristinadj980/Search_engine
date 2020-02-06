a79c5fab9302ba489003f419f12c44d8151b75be
Maja Dragojlovic <majadragojlovic@yahoo.com>
2020-02-06 21:08:48 +0100

    #parsiranje_skupa_HTML_dokumenata Ubaceni test podaci, parser.py, implementirana funkcija koja pronalazi sve html dokumente unutar foldera

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import sys
 import os
+from library.parser import Parser
 from os import path
 
 
 def main():
+    filepaths = None
+
     while True:
         print("##################################")
         print("Menu")
@@ -20,9 +23,12 @@ def main():
 
         if option == 1:
             directory = choose_directory()
-
             if not directory:
                 continue
+            else:
+                filepaths = find_html_files(directory)
+
+            parse_documents(filepaths)
 
         elif option == 0:
             sys.exit('Bye')
@@ -31,13 +37,38 @@ def main():
 
 
 def choose_directory():
+    # temp = "C:\\Users\\Lenovo\\Desktop\\oisisi_python\\test-skup"
     dir = input('Enter directory absolute path: ')
+    # dir = temp
 
     if not os.path.isdir(dir):
         print('Path does not exist or it is a file, not a directory')
         return None
 
     return dir
+
+
+def find_html_files(dir):
+    filepaths = []
+    for subdir, dirs, files in os.walk(dir):
+        for file in files:
+            filepath = subdir + os.sep + file
+
+            if filepath.endswith(".html"):
+                filepaths.append(filepath)
+
+    return filepaths
+
+
+def parse_documents(paths):
+    all_words = dict()
+    all_links = dict()
+
+    for path in paths:
+        parser = Parser()
+        links, words = parser.parse(path)
+        all_words[path] = words
+        all_links[path] = links
 
 
 main()
